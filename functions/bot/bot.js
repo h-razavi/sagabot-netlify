074a0bd0a71d0cd80b1515e6a2318e84b02baf53
Hossain Razavi <hossain.razavi@gmail.com>
2023-06-20 14:14:12 +0330

    update bot logic initial

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -3,14 +3,33 @@ require("dotenv").config();
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
 bot.start(ctx => {
-  console.log("Received /start command")
+    const message =
+    "سلام؛ به ربات تلگرامی پادکست ساگا خوش اومدید! \n پیام خودتون رو به شکل متنی یا صوتی برای ما بفرستید";
   try {
-    return ctx.reply("Hi")
+    return ctx.reply(message)
   } catch (e) {
     console.error("error in start action:", e)
     return ctx.reply("Error occured")
   }
 })
+
+bot.on('message',(ctx)=>{
+    const chatId = process.env.CHAT_ID;
+    const botCommands = "/contribute"
+
+    if(ctx.message.text===botCommands){
+      ctx.reply(contMessage)
+    }
+
+    if(ctx.message.text||ctx.message.voice){
+        ctx.telegram.forwardMessage(chatId,ctx.chat.id,ctx.message.message_id)
+    } else {
+        ctx.reply("لطفاً فقط پیام متنی یا صوتی بفرستید!")
+    }
+})
+
+bot.command('contribute', Telegraf.reply(contMessage));
+
 
 exports.handler = async event => {
   try {
