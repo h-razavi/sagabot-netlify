42fce0e586f526f4d646894895be20b107bb4609
Hossain Razavi <hossain.razavi@gmail.com>
2023-06-21 10:55:55 +0330

    fix button row issue

--- data/data.js
+++ data/data.js
@@ -9,14 +9,14 @@ const data = {
     "💬 هر پیام متنی یا صوتی که برای این ربات بفرستید، مستقیماً به دست ما می‌رسه!\n/start - برای شروع ارسال پیام\n/contribute - برای کمک کردن به ما در توسعه این ربات تلگرامی\n/podlinks - لینک دسترسی پادکست ساگا در اپلیکیشن‌های پادگیر"
     },
     podApps : [
-      {text:"iTunes" , url:"https://itunes.apple.com/us/podcast/saga/id1436192969"},
-      {text:"Google Podcasts" , url:"https://www.google.com/podcasts?feed=aHR0cHM6Ly9hbmNob3IuZm0vcy82Y2Q0Y2E4L3BvZGNhc3QvcnNz"},
-      {text:"Castbox" , url:"https://castbox.fm/channel/id4853129"},
-      {text:"OverCast" , url:"https://overcast.fm/itunes1436192969/saga"},
-      {text:"PocketCasts" , url:"https://play.pocketcasts.com/web/podcasts/index#/podcasts/show/8df01270-99a2-0136-7b93-27f978dac4db"},
-      {text:"PodBean" , url:"https://www.podbean.com/podcast-detail/kyqh8-7525e/Saga-Podcast"},
-      {text:"Spotify" , url:"https://open.spotify.com/show/5uX9ARaSvndEuevFpF2cBn"},
-      {text:"Breakr" , url:"https://www.breaker.audio/saga-2"},
+      [{text:"iTunes" , url:"https://itunes.apple.com/us/podcast/saga/id1436192969"}],
+      [{text:"Google Podcasts" , url:"https://www.google.com/podcasts?feed=aHR0cHM6Ly9hbmNob3IuZm0vcy82Y2Q0Y2E4L3BvZGNhc3QvcnNz"}],
+      [{text:"Castbox" , url:"https://castbox.fm/channel/id4853129"}],
+      [{text:"OverCast" , url:"https://overcast.fm/itunes1436192969/saga"}],
+      [{text:"PocketCasts" , url:"https://play.pocketcasts.com/web/podcasts/index#/podcasts/show/8df01270-99a2-0136-7b93-27f978dac4db"}],
+      [{text:"PodBean" , url:"https://www.podbean.com/podcast-detail/kyqh8-7525e/Saga-Podcast"}],
+      [{text:"Spotify" , url:"https://open.spotify.com/show/5uX9ARaSvndEuevFpF2cBn"}],
+      [{text:"Breakr" , url:"https://www.breaker.audio/saga-2"}],
   ],
 };
 
