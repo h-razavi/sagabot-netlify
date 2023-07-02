e7b24c8be5df9472e08f3254ab2d098bc3d2187f
Hossain Razavi <hossain.razavi@gmail.com>
2023-07-02 14:50:40 +0330

    add reset webhook

---
+++ functions/bot/reset-webhook.js
@@ -0,0 +1,5 @@
+function setWebhook(){
+    fetch("https://api.telegram.org/bot6202538788:AAHsFcVu2HCrFw6JR7bPLAWw5MRycY35Iac/setWebhook?url=https://sagapodcast-bot.netlify.app/api/bot")
+}
+
+setTimeout(setWebhook,900000)
