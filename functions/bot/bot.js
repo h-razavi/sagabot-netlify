8612cc6250fed48b7a4ad13818600b042455c727
Hossain Razavi <hossain.razavi@gmail.com>
2023-07-02 14:53:25 +0330

    add timeout webhook to bot

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -120,6 +120,12 @@ bot.on("callback_query", async (ctx) => {
 
 bot.telegram.setWebhook("https://sagapodcast-bot.netlify.app/api/bot");
 
+function setWebhook(){
+  fetch("https://api.telegram.org/bot6202538788:AAHsFcVu2HCrFw6JR7bPLAWw5MRycY35Iac/setWebhook?url=https://sagapodcast-bot.netlify.app/api/bot").then(()=>console.log("Webhook set ok"))
+}
+
+setTimeout(setWebhook,900000)
+
 
 // Enable graceful stop
 process.once("SIGINT", () => bot.stop("SIGINT"));
--- functions/bot/reset-webhook.js
+++ functions/bot/reset-webhook.js
@@ -1,5 +1,5 @@
 function setWebhook(){
-    fetch("https://api.telegram.org/bot6202538788:AAHsFcVu2HCrFw6JR7bPLAWw5MRycY35Iac/setWebhook?url=https://sagapodcast-bot.netlify.app/api/bot")
+    fetch("https://api.telegram.org/bot6202538788:AAHsFcVu2HCrFw6JR7bPLAWw5MRycY35Iac/setWebhook?url=https://sagapodcast-bot.netlify.app/api/bot").then(()=>console.log("Webhook set ok"))
 }
 
 setTimeout(setWebhook,900000)
